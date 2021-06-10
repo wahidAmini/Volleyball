2aa232084c3eed7cc065ab4be7ee87ff5b510a68
Wahid Amini <WA@go2cam.net>
2021-06-10 14:54:32 +0200

    BootStrap and tables added to the index veiw page and display the players

--- src/Views/players/index.php
+++ src/Views/players/index.php
@@ -2,19 +2,37 @@
 <html lang="en">
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>players</title>
 </head>
 
-<body>
-    <h1>Liste des players</h1>
-    <ul>
-        <?php foreach ($players as $player) : ?>
-            <li><?= $player->name ?></li>
-        <?php endforeach; ?>
-    </ul>
+<body class="container">
+    <h1 class="text-center">Liste des players</h1>
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Team</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($players as $player) : ?>
+            <tr>
+                <td><?= $player->id ?></td>
+                <td><?= $player->name ?></td>
+                <td><?= $player->age ?></td>
+                <td><?= $player->team ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+        </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- src/index.php
+++ src/index.php
@@ -7,8 +7,8 @@ use Core\Router\Router;
 require 'autoload.php'; // Pas toucher svp
 require 'routes.php';
 
-/* $router = new Router();
-$router->run(); */
+// $router = new Router();
+// $router->run();
 echo "<h2>Hello Heros !</h2>";
 
 // $controller = new AnimalController();
