bfbe38d9ee91ba17ffe1134db0ca707ddf6b3b75
Wahid Amini <WA@go2cam.net>
2021-06-14 21:44:50 +0200

    home page title updated

--- src/Views/home.php
+++ src/Views/home.php
@@ -7,12 +7,12 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title>Home</title>
 </head>
 <body>
    <nav class="navbar navbar-expand-lg navbar-light bg-light">
    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Blog Volleball</a>
+      <a class="navbar-brand" href="/">Blog Volleyball</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
       </button>
